9f7f4000947f9791c1b5804081a837524f29f74f
sabrinagalvan <sabrinagalvans2@gmail.com>
2024-05-14 22:21:24 -0300

    adicionando continuação do projeto de estudo

---
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,32 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.Especialidade;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+}
